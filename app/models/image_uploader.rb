2901b066c688599d57371bd0f3b709a3a67fdb27
Janko Marohnić <janko.marohnic@gmail.com>
2015-12-11 11:03:23 +0100

    The MIME type will always be "image/jpeg"
    The MIME type for both .jpg and .jpeg images will always be
    "image/jpeg".

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -11,7 +11,7 @@ class ImageUploader < Shrine
 
   Attacher.validate do
     validate_max_size 2.megabytes, message: 'is too large (max is 2 MB)'
-    validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+    validate_mime_type_inclusion ['image/jpeg', 'image/png', 'image/gif']
   end
 
   def process(io, context)
