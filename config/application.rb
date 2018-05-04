e32b256041dd20bfb28bf196d998f822b88889b9
Janko Marohnić <janko.marohnic@gmail.com>
2018-05-04 20:52:46 +0200

    Update Rails to 5.2

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module ShrineRailsExample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.active_record.sqlite3.represent_boolean_as_integer = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
