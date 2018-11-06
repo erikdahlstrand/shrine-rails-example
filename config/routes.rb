d529f5584832edd980607bdaff332337e9ed442e
Janko Marohnić <janko.marohnic@gmail.com>
2018-11-06 01:21:36 +0100

    Update for Uppy 0.28.0

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :albums
 
   if Rails.env.production?
-    mount Shrine.presign_endpoint(:cache) => "/presign"
+    mount Shrine.presign_endpoint(:cache) => "/s3/params"
   else
     # In development and test environment we're using filesystem storage
     # for speed, so on the client side we'll upload files to our app.
